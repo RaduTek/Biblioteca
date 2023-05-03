44947cb0d3270ab334da139faa9cf916ec72f625
RaduTek <radutek13@gmail.com>
2023-05-03 17:54:37 +0300

    Administrarea imprumuturilor

--- imprumuturi.php
+++ imprumuturi.php
@@ -4,17 +4,49 @@ include_once('template_1.php');
 
 $titlu_pagina = "Împrumuturi";
 
+$page_msg = "ok";
+
+// Stari:
+
+$stari = [
+    "asteptare" => "În așteptare",
+    "predat" => "Predat",
+    "anulat" => "Anulat",
+    "returnat" => "Returnat",
+];
+
+function modifica_stare_imprumut($id, $stare) {
+    global $bazadate;
+    $stmt = $bazadate -> prepare("UPDATE biblioteca_imprumuturi SET stare = ? WHERE id = ?");
+    $stmt -> bind_param("ss", $stare, $id);
+    $status = $stmt -> execute();
+    $page_msg = "modificare_stare_" . ($status ? "ok" : "fail");
+}
+
+if (isset($_SESSION['user_tip']) && $_SESSION['user_tip'] = 'admin') {
+
+    if (isset($_GET['preda'])) {
+        modifica_stare_imprumut($_GET['preda'], 'predat');
+    } else if (isset($_GET['anuleaza'])) {
+        modifica_stare_imprumut($_GET['anuleaza'], 'anulat');
+    } else if (isset($_GET['returneaza'])) {
+        modifica_stare_imprumut($_GET['returneaza'], 'returnat');
+    }
+
+}
+
 include_once('template_2.php');
 
 ?>
 
 <div id="continut">
-    <h1>Împrumuturi</h1>
+<?php if (isset($_SESSION['user_tip']) && $_SESSION['user_tip'] = 'admin') { ?>
+
+    <h1>Împrumuturi active</h1>
     <div class="table" style="text-align: center">
         <table>
             <thead>
                 <tr>
-                    <th>Cod Împrumut</th>
                     <th>Carte</th>
                     <th>Utilizator</th>
                     <th>Data împrumutului</th>
@@ -24,18 +56,39 @@ include_once('template_2.php');
                 </tr>
             </thead>
             <tbody>
+            <?php 
+                $stmt = $bazadate -> prepare("SELECT biblioteca_imprumuturi.*, biblioteca_useri.nume AS nume_user, biblioteca_carti.titlu AS titlu_carte FROM biblioteca_imprumuturi 
+                    INNER JOIN biblioteca_useri ON biblioteca_imprumuturi.user=biblioteca_useri.id 
+                    INNER JOIN biblioteca_carti ON biblioteca_imprumuturi.carte=biblioteca_carti.id 
+                    WHERE biblioteca_imprumuturi.stare != 'returnat' AND biblioteca_imprumuturi.stare != 'anulat'
+                    ORDER BY biblioteca_imprumuturi.id");
+                $stmt -> execute();
+                $result = $stmt -> get_result();
+                while ($row = $result->fetch_assoc()) {
+            ?>
                 <tr>
-                    <td>012345abc</td>
-                    <td><a href="carte.php?id="></a></td>
-                    <td>Nume Utilizator</td>
-                    <td>01.01.2023</td>
-                    <td>03.01.2023</td>
-                    <td>In Asteptare</td>
-                    <td><a href="imprumut.php?finalizare="><button class="btn">Finalizează</button></a></td>
+                    <td><a href="carte.php?id=<?php echo $row['carte']; ?>"><?php echo $row['titlu_carte']; ?></a></td>
+                    <td><?php echo $row['nume_user']; ?></td>
+                    <td><?php echo $row['inceput']; ?></td>
+                    <td><?php echo $row['sfarsit']; ?></td>
+                    <td><?php echo $stari[$row['stare']]; ?></td>
+                    <td>
+                        <?php if($row['stare'] == 'predat') { ?>
+                            <a href="imprumuturi.php?returneaza=<?php echo $row['id']; ?>"><button class="btn">Returnează</button></a>
+                        <?php } else { ?> 
+                            <a href="imprumuturi.php?preda=<?php echo $row['id']; ?>"><button class="btn">Predă</button></a>
+                        <?php } ?>
+                        <a href="imprumuturi.php?anuleaza=<?php echo $row['id']; ?>"><button class="btn">Anulează</button></a>
+                    </td>
                 </tr>
+            <?php } ?>
             </tbody>
         </table>
     </div>
+    
+<?php } else { ?>
+    <div class="alertbox btn-red">Trebuie să fiți autentificat cu un cont administrativ pentru a avea acces la această funcție!</div><br/>
+<?php } ?>
 </div>
 
 <?php include_once('template_3.php'); ?>
